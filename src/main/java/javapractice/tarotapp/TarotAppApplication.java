4a8988061a1c3491c7dc94ea82b5db26c2b3d1e6
BrookeBryski <brookeabryski@gmail.com>
2021-12-08 10:28:06 -0600

    initial commit

---
+++ src/main/java/javapractice/tarotapp/TarotAppApplication.java
@@ -0,0 +1,13 @@
+package javapractice.tarotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarotAppApplication.class, args);
+	}
+
+}
