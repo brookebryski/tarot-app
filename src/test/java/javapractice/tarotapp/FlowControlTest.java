0786e05686171c69262868bdf78c3e51229aaf4c
BrookeBryski <brookeabryski@gmail.com>
2021-12-09 12:29:41 -0600

    finished flow control test with major arcana

--- src/test/java/javapractice/tarotapp/FlowControlTest.java
+++ src/test/java/javapractice/tarotapp/FlowControlTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class FlowControlTest {
     private  IMajorArcanaValidator _majorArcanaValidator;
@@ -20,7 +20,43 @@ class FlowControlTest {
 
     @Test
     public void givenTheFoolCallIsMajorArcanaOnce() {
+        // Given: I am a user
+        // When: I enter the string "the fool"
+        when(_majorArcanaValidator.isMajorArcana("the fool")).thenReturn(true);
+        _flowControl.run("the fool");
+        // Then: Call isMajorArcana() once
+        verify(_majorArcanaValidator, times(1)).isMajorArcana("the fool");
+    }
 
+    @Test
+    public void givenTheFoolCallMajorArcanaOnce() {
+        // Given: I am a user
+        // When: I enter the string "the fool"
+        when(_majorArcanaValidator.isMajorArcana("the fool")).thenReturn(true);
+        when(_majorArcana.generateTraits("the fool")).thenReturn(anyString());
+        _flowControl.run("the fool");
+        // Then: Call MajorArcana() once
+        verify(_majorArcana, times(1)).generateTraits("the fool");
+    }
+
+    @Test
+    public void givenBobCallISMajorArcanaOnce() {
+        // Given: I am a user
+        // When: I enter the string "bob"
+        when(_majorArcanaValidator.isMajorArcana("bob")).thenReturn(false);
+        _flowControl.run("bob");
+        // Then: Call isMajorArcana() once
+        verify(_majorArcanaValidator, times(1)).isMajorArcana("bob");
+    }
+
+    @Test
+    public void givenBobDoNotCallMajorArcana() {
+        // Given: I am a user
+        // When: I enter the string "bob"
+        when(_majorArcanaValidator.isMajorArcana("bob")).thenReturn(false);
+        _flowControl.run("bob");
+        // Then: Call MajorArcana() zero times
+        verify(_majorArcana, never()).generateTraits(anyString());
     }
 
 }
